12536de2f1233cef3c4a5f1173a616d2b8085ced
sagi <58301845+sagidev@users.noreply.github.com>
2022-05-06 22:25:39 +0200

    ASD2

---
+++ Hashtable.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+class Hashtable
+{
+public:
+    int key, distance;
+    string chain;
+
+    //Hashtable(int kkey, int ddistance, string cchain)
+    //{
+    //    key = kkey;
+    //    disctance = ddistance;
+    //    chain = cchain;
+    //}
+    string Print()
+    {
+        return (key + " " + chain);
+        //cout << key << " " << chain << endl;
+    }
+};
+
+void DoHashtable(string fileIn, string fileOut)
+{
+    ifstream dane(fileIn);
+    ofstream save(fileOut);
+    string script;
+    int size, cases, tests = 0;
+    dane >> cases;
+    dane >> script >> size;
+    Hashtable* arr = new Hashtable[size];
+    cout << "Hashujemy dla " << cases << " przypadkow";
+    while (tests < cases && !dane.eof())
+    {
+        dane >> script;
+        
+        if (script == "size")
+        {
+            dane >> size;
+            arr = new Hashtable[size];
+        }
+        if (script == "print")
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (arr[i].key != NULL)
+                {
+                    save << i << " " << arr[i].Print();
+                }
+            }
+        }
+        if (script == "add")
+        {
+            dane >> arr->key >> arr->chain;
+            int index = arr->key % size;
+            int distance = 0;
+            while (arr[index].key != NULL)
+            {
+                if (index == size - 1)
+                {
+                    index = -1;
+                }
+                index++;
+                distance++;
+            }
+            arr[index].key = arr->key;
+            arr[index].chain = arr->chain;
+            arr[index].distance = distance;
+        }
+        if (script == "stop")
+        {
+            tests++;
+        }
+        if (script == "delete")
+        {
+            dane >> arr->key;
+            int index = arr->key % size;
+            while (arr[index].key != NULL)
+            {
+                if (index == size - 1)
+                {
+                    index = -1;
+                }
+                index++;
+            }
+            arr[index].key = NULL;
+            arr[index].chain = "";
+            arr[index].distance = NULL;
+        }
+    }
+    dane.close();
+    save.close();
+    delete[] arr;
+}
+
+int main()
+{
+    DoHashtable("hashtablein.txt", "hashtableout.txt");
+}
+
---
+++ Heapsort.cpp
@@ -0,0 +1,97 @@
+﻿// DT_Heap.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+void Heapify(int arr[], int i, int max)
+{
+    int largest = 0;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+    if (left<max && arr[left]>arr[i])
+    {
+        largest = left;
+    }
+    else
+    {
+        largest = i;
+    }
+
+    if (right<max && arr[right]>arr[largest])
+    {
+        largest = right;
+    }
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+        Heapify(arr, largest, max);
+
+    }
+}
+
+void HeapSort(int arr[], int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+    {
+        Heapify(arr, i, n);
+    }
+
+    for (int i = n - 1; i >= 1; i--)
+    {
+        swap(arr[0], arr[i]);
+        Heapify(arr, 0, i);
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << ", ";
+    }
+}
+
+void readInput(string path)
+{
+    ifstream input_file;
+    input_file.open(path);
+    string line;
+    istringstream ss(line);
+    string del;
+    while (getline(input_file, del, ' '))
+    {
+
+    }
+}
+
+string howManyTests(string path)
+{
+    ifstream input_file;
+    input_file.open(path);
+    string line;
+    while (getline(input_file, line)) {
+        return line;
+        break;
+    }
+}
+
+int main()
+{
+    string path = "input.txt";
+    int numberOfTries;
+    numberOfTries = stoi(howManyTests(path));
+    for (int i = 0; i < numberOfTries; i++)
+    {
+        int arr[] = { 5,5,1,8,3,7 };//predefiniowana tabela
+        int n = sizeof(arr) / sizeof(arr[0]);
+        HeapSort(arr, n);
+        printArray(arr, n);
+        cout << endl;
+    }
+
+}
+
