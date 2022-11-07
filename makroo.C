336a05e18f3dc217358bccdd4634e342a9176d71
sagi <58301845+sagidev@users.noreply.github.com>
2022-11-07 12:46:35 +0100

    Create makroo.C

---
+++ makroo.C
@@ -0,0 +1,65 @@
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+void makroo()
+{
+    int n = 2000, i;
+    float  x1[2000];
+    ifstream d;
+    d.open("D:\\Users\\UL0255135\\source\\repos\\ConsoleApplication1\\ConsoleApplication1\\dane.txt");
+
+    // pod windows pelna sciezka z podwojnymi \	\
+  //   d.open("D:\\Users\\moje\\dane.txt");
+    for (i = 0; i < 2000; i++)
+    {
+        d >> x1[i];
+    }
+    d.close();
+    // deklarowanie histogramu "jednowymiarowego"  .... liczba klas, przedział
+    TH1F* hi1 = new TH1F("hi1", "nazwa histogramu", 40, -1.6, 0);
+    // wypelnianie histogramu jednowymiarowego
+    for (i = 0; i < 2000; i++)
+    {
+        hi1->Fill(x1[i]);
+    }
+
+    /*
+    // deklarowanie histogramu "dwuwymiarowego"  .... liczba klas, przedział najpierw po zmiennej x, potem po zmiennej y
+   TH2F *imd2 = new TH2F("imd2","jestem histogramem-2wym ",40,-5.,400.,20,-5,400.);
+
+       // wypelnianie histogramu dwuwymiarowego
+    for(i=0;i<3000;i=i+2)
+        {
+          hi2->Fill(x1[i],x1[i+1]);
+        }
+    */
+
+
+    // stworzenie canvy do rysunku
+
+    TCanvas* test = new TCanvas("test", "test");
+    test->SetFillColor(0);
+
+    //kolory i style histogramu (mozna bez	
+    hi1->SetFillColor(2);
+    hi1->SetFillStyle(3002);
+    // rysowanie histogramu
+    hi1->Draw();
+
+    //definicja funkcji do modelowania
+     // f4 wlasna funkcja, w nawisach kwadratowych nieznane parametry, ktorych szuka program po uruchomieniu fit jesli modelowanie jedna funkcja to jak w przypadku f4
+    TF1* f4 = new TF1("f4", "[0]*exp(-x*[1])", -0, 400);
+    hi1->Fit("pol4");
+    /*
+        //f1,f2,f3 jesli modelowanie kawalkami ( ostatnie liczby to zakresy )
+   TF1* f1 = new TF1("f1","expo",0,19);
+   hi1->Fit(f1,"R+");
+   TF1* f2 = new TF1("f2","expo",19,43.5);
+   hi1->Fit(f2,"R+");
+   TF1* f3 = new TF1("f3","pol9",43.5,400);
+   hi1->Fit(f3,"R+");
+    */
+}
